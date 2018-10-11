98d0062cabc99e950a57109bd115b53c5a0d231c
TKNgu <kyvophonge@gmail.com>
2018-10-11 14:53:22 +0700

    Test Lib glm

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,6 @@
 #include "glm/gtc/type_ptr.hpp"
 
 int main(int argc, char **argv) {
-    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-    glm::mat4 trans;
-    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-    vec = trans * vec;
-    std::cout << vec.x << vec.y << vec.z << std::endl;
-
     try {
         Window window(800, 600, "LearnOpenGL");
 
@@ -78,6 +72,30 @@ int main(int argc, char **argv) {
         shader->setInt("texture1", 0);
         shader->setInt("texture2", 1);
 
+        // glm::mat4 trans(1.0f);
+        // trans = glm::scale( trans, glm::vec3(0.5, -0.5, 0.5f));
+        // trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        // glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+        // vec = trans * vec;
+        // std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+
+        // unsigned int transformLoc = glGetUniformLocation(shader->id, "transform");
+        // glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        // float* matrix = glm::value_ptr(trans);
+        // for(int indexY = 0; indexY < 4; indexY++) {
+        //     for(int indexX = 0; indexX < 4; indexX++) {
+        //         std::cout << matrix[indexY * 4 + indexX] << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
+
+        unsigned int color = glGetUniformLocation(shader->id, "ourColor");
+        glUniform4f(color, 0.0f, 1.0, 0.0f, 1.0f);
+        glm::vec3 location(0.0);
+        glm::vec3 speed(1.0f, -1.0f, 1.0f);
+        glm::mat4 trans(1.0f);
+
         while(!glfwWindowShouldClose(window.getWindow())) {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
@@ -85,6 +103,29 @@ int main(int argc, char **argv) {
             sin(glfwGetTime()) > 0 ? glBindTexture(GL_TEXTURE_2D, texture) : glBindTexture(GL_TEXTURE_2D, 0);
             glActiveTexture(GL_TEXTURE1);
             glBindTexture(GL_TEXTURE_2D, texture1);
+
+            location += glm::vec3((float)sin(glfwGetTime()) / 10.0f, (float)sin(glfwGetTime()) / 10.0f, 0.0f) * speed;
+            if(location.x > 0.5 || location.x < -0.5) {
+                speed.x = -speed.x;
+            }
+            if(location.y > 0.5 || location.y < -0.5) {
+                speed.y = -speed.y;
+            }
+            // std::cout << location.x << " " << location.y << " " << location.z << std::endl;
+
+            trans = glm::rotate(trans, (float)sin(glfwGetTime()) / 5.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+            trans = glm::translate(trans, glm::vec3((float)sin(glfwGetTime()) / 10.0f, (float)sin(glfwGetTime()) / 10.0f, 0.0f) * speed);
+
+            float* matrix = glm::value_ptr(trans);
+            for(int indexY = 0; indexY < 4; indexY++) {
+                for(int indexX = 0; indexX < 4; indexX++) {
+                    std::cout << matrix[indexY * 4 + indexX] << " ";
+                }
+                std::cout << std::endl;
+            }
+
+            unsigned int transformLoc = glGetUniformLocation(shader->id, "transform");
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
             glBindVertexArray(VAO);
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
