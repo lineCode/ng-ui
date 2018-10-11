79b315ef962840c3b620867eadfaa4b7e8d55082
TKNgu <kyvophonge@gmail.com>
2018-10-11 14:59:01 +0700

    Demo

--- data/shader.fs
+++ data/shader.fs
@@ -4,7 +4,8 @@ in vec2 TexCoord;
 out vec4 FragColor;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
+uniform vec4 ourColor;
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * ourColor;
 }
