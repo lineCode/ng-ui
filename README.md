4a0a84c70c14e83e9d900e3c4290bd1f52e08f1a
TKNgu <kyvophonge@gmail.com>
2018-10-15 08:20:43 +0700

    back

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Simple UI build from OpenGL.
 
 # 11-09-2018
 Start project.
-Add ssh key for pull, push and chang to ssh path.<br/>
+Add ssh key for pull, push and chang to ssh path.
 $ git remote set-url origin git@github.com:tkngu/ng-ui.git
 
 # 05-10-2018
--- data/shader.fs
+++ data/shader.fs
@@ -4,8 +4,7 @@ in vec2 TexCoord;
 out vec4 FragColor;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform vec4 ourColor;
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * ourColor;
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
